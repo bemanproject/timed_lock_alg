bff314214ca10e565aef498cb096efb01b3d5f0c
Edward Nolan <eddiejnolan@gmail.com>
2025-09-30 12:24:27 -0400

    Remove cookiecutter; bump infra beman-submodule; update CMake style for pre-commit
    Cookiecutter is intended to help users create new libraries, but isn't
    needed outside of exemplar; this commit removes it.
    
    infra/cmake/use-fetch-content.cmake became out of sync with the
    upstream due to a bad find-and-replace; this commit brings it back
    into alignment with infra and updates the other files in infra to the
    latest version.
    
    Finally, pre-commit was failing because some CMake formatting
    disagreed with the gersemi style checker used by pre-commit; this
    commit changes applies that tool's changes.

--- infra/cmake/use-fetch-content.cmake
+++ infra/cmake/use-fetch-content.cmake
@@ -2,200 +2,184 @@ cmake_minimum_required(VERSION 3.24)
 
 include(FetchContent)
 
-if(NOT BEMAN_TIMED_LOCK_ALG_LOCKFILE)
-    set(BEMAN_TIMED_LOCK_ALG_LOCKFILE
+if(NOT BEMAN_EXEMPLAR_LOCKFILE)
+    set(BEMAN_EXEMPLAR_LOCKFILE
         "lockfile.json"
         CACHE FILEPATH
-        "Path to the dependency lockfile for the Beman Timed Lock Algorithms."
+        "Path to the dependency lockfile for the Beman Exemplar."
     )
 endif()
 
-set(BemanTimedLockAlg_projectDir "${CMAKE_CURRENT_LIST_DIR}/../..")
-message(
-    TRACE
-    "BemanTimedLockAlg_projectDir=\"${BemanTimedLockAlg_projectDir}\""
-)
+set(BemanExemplar_projectDir "${CMAKE_CURRENT_LIST_DIR}/../..")
+message(TRACE "BemanExemplar_projectDir=\"${BemanExemplar_projectDir}\"")
 
-message(
-    TRACE
-    "BEMAN_TIMED_LOCK_ALG_LOCKFILE=\"${BEMAN_TIMED_LOCK_ALG_LOCKFILE}\""
-)
+message(TRACE "BEMAN_EXEMPLAR_LOCKFILE=\"${BEMAN_EXEMPLAR_LOCKFILE}\"")
 file(
     REAL_PATH
-    "${BEMAN_TIMED_LOCK_ALG_LOCKFILE}"
-    BemanTimedLockAlg_lockfile
-    BASE_DIRECTORY "${BemanTimedLockAlg_projectDir}"
+    "${BEMAN_EXEMPLAR_LOCKFILE}"
+    BemanExemplar_lockfile
+    BASE_DIRECTORY "${BemanExemplar_projectDir}"
     EXPAND_TILDE
 )
-message(DEBUG "Using lockfile: \"${BemanTimedLockAlg_lockfile}\"")
+message(DEBUG "Using lockfile: \"${BemanExemplar_lockfile}\"")
 
 # Force CMake to reconfigure the project if the lockfile changes
 set_property(
-    DIRECTORY "${BemanTimedLockAlg_projectDir}"
+    DIRECTORY "${BemanExemplar_projectDir}"
     APPEND
-    PROPERTY CMAKE_CONFIGURE_DEPENDS "${BemanTimedLockAlg_lockfile}"
+    PROPERTY CMAKE_CONFIGURE_DEPENDS "${BemanExemplar_lockfile}"
 )
 
 # For more on the protocol for this function, see:
 # https://cmake.org/cmake/help/latest/command/cmake_language.html#provider-commands
-function(BemanTimedLockAlg_provideDependency method package_name)
+function(BemanExemplar_provideDependency method package_name)
     # Read the lockfile
-    file(READ "${BemanTimedLockAlg_lockfile}" BemanTimedLockAlg_rootObj)
+    file(READ "${BemanExemplar_lockfile}" BemanExemplar_rootObj)
 
-    # Get the "dependencies" field and store it in BemanTimedLockAlg_dependenciesObj
+    # Get the "dependencies" field and store it in BemanExemplar_dependenciesObj
     string(
         JSON
-        BemanTimedLockAlg_dependenciesObj
-        ERROR_VARIABLE BemanTimedLockAlg_error
-        GET "${BemanTimedLockAlg_rootObj}"
+        BemanExemplar_dependenciesObj
+        ERROR_VARIABLE BemanExemplar_error
+        GET "${BemanExemplar_rootObj}"
         "dependencies"
     )
-    if(BemanTimedLockAlg_error)
-        message(
-            FATAL_ERROR
-            "${BemanTimedLockAlg_lockfile}: ${BemanTimedLockAlg_error}"
-        )
+    if(BemanExemplar_error)
+        message(FATAL_ERROR "${BemanExemplar_lockfile}: ${BemanExemplar_error}")
     endif()
 
-    # Get the length of the libraries array and store it in BemanTimedLockAlg_dependenciesObj
+    # Get the length of the libraries array and store it in BemanExemplar_dependenciesObj
     string(
         JSON
-        BemanTimedLockAlg_numDependencies
-        ERROR_VARIABLE BemanTimedLockAlg_error
-        LENGTH "${BemanTimedLockAlg_dependenciesObj}"
+        BemanExemplar_numDependencies
+        ERROR_VARIABLE BemanExemplar_error
+        LENGTH "${BemanExemplar_dependenciesObj}"
     )
-    if(BemanTimedLockAlg_error)
-        message(
-            FATAL_ERROR
-            "${BemanTimedLockAlg_lockfile}: ${BemanTimedLockAlg_error}"
-        )
+    if(BemanExemplar_error)
+        message(FATAL_ERROR "${BemanExemplar_lockfile}: ${BemanExemplar_error}")
     endif()
 
-    if(BemanTimedLockAlg_numDependencies EQUAL 0)
+    if(BemanExemplar_numDependencies EQUAL 0)
         return()
     endif()
 
     # Loop over each dependency object
-    math(
-        EXPR
-        BemanTimedLockAlg_maxIndex
-        "${BemanTimedLockAlg_numDependencies} - 1"
-    )
-    foreach(BemanTimedLockAlg_index RANGE "${BemanTimedLockAlg_maxIndex}")
-        set(BemanTimedLockAlg_errorPrefix
-            "${BemanTimedLockAlg_lockfile}, dependency ${BemanTimedLockAlg_index}"
+    math(EXPR BemanExemplar_maxIndex "${BemanExemplar_numDependencies} - 1")
+    foreach(BemanExemplar_index RANGE "${BemanExemplar_maxIndex}")
+        set(BemanExemplar_errorPrefix
+            "${BemanExemplar_lockfile}, dependency ${BemanExemplar_index}"
         )
 
-        # Get the dependency object at BemanTimedLockAlg_index
-        # and store it in BemanTimedLockAlg_depObj
+        # Get the dependency object at BemanExemplar_index
+        # and store it in BemanExemplar_depObj
         string(
             JSON
-            BemanTimedLockAlg_depObj
-            ERROR_VARIABLE BemanTimedLockAlg_error
-            GET "${BemanTimedLockAlg_dependenciesObj}"
-            "${BemanTimedLockAlg_index}"
+            BemanExemplar_depObj
+            ERROR_VARIABLE BemanExemplar_error
+            GET "${BemanExemplar_dependenciesObj}"
+            "${BemanExemplar_index}"
         )
-        if(BemanTimedLockAlg_error)
+        if(BemanExemplar_error)
             message(
                 FATAL_ERROR
-                "${BemanTimedLockAlg_errorPrefix}: ${BemanTimedLockAlg_error}"
+                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
             )
         endif()
 
-        # Get the "name" field and store it in BemanTimedLockAlg_name
+        # Get the "name" field and store it in BemanExemplar_name
         string(
             JSON
-            BemanTimedLockAlg_name
-            ERROR_VARIABLE BemanTimedLockAlg_error
-            GET "${BemanTimedLockAlg_depObj}"
+            BemanExemplar_name
+            ERROR_VARIABLE BemanExemplar_error
+            GET "${BemanExemplar_depObj}"
             "name"
         )
-        if(BemanTimedLockAlg_error)
+        if(BemanExemplar_error)
             message(
                 FATAL_ERROR
-                "${BemanTimedLockAlg_errorPrefix}: ${BemanTimedLockAlg_error}"
+                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
             )
         endif()
 
-        # Get the "package_name" field and store it in BemanTimedLockAlg_pkgName
+        # Get the "package_name" field and store it in BemanExemplar_pkgName
         string(
             JSON
-            BemanTimedLockAlg_pkgName
-            ERROR_VARIABLE BemanTimedLockAlg_error
-            GET "${BemanTimedLockAlg_depObj}"
+            BemanExemplar_pkgName
+            ERROR_VARIABLE BemanExemplar_error
+            GET "${BemanExemplar_depObj}"
             "package_name"
         )
-        if(BemanTimedLockAlg_error)
+        if(BemanExemplar_error)
             message(
                 FATAL_ERROR
-                "${BemanTimedLockAlg_errorPrefix}: ${BemanTimedLockAlg_error}"
+                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
             )
         endif()
 
-        # Get the "git_repository" field and store it in BemanTimedLockAlg_repo
+        # Get the "git_repository" field and store it in BemanExemplar_repo
         string(
             JSON
-            BemanTimedLockAlg_repo
-            ERROR_VARIABLE BemanTimedLockAlg_error
-            GET "${BemanTimedLockAlg_depObj}"
+            BemanExemplar_repo
+            ERROR_VARIABLE BemanExemplar_error
+            GET "${BemanExemplar_depObj}"
             "git_repository"
         )
-        if(BemanTimedLockAlg_error)
+        if(BemanExemplar_error)
             message(
                 FATAL_ERROR
-                "${BemanTimedLockAlg_errorPrefix}: ${BemanTimedLockAlg_error}"
+                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
             )
         endif()
 
-        # Get the "git_tag" field and store it in BemanTimedLockAlg_tag
+        # Get the "git_tag" field and store it in BemanExemplar_tag
         string(
             JSON
-            BemanTimedLockAlg_tag
-            ERROR_VARIABLE BemanTimedLockAlg_error
-            GET "${BemanTimedLockAlg_depObj}"
+            BemanExemplar_tag
+            ERROR_VARIABLE BemanExemplar_error
+            GET "${BemanExemplar_depObj}"
             "git_tag"
         )
-        if(BemanTimedLockAlg_error)
+        if(BemanExemplar_error)
             message(
                 FATAL_ERROR
-                "${BemanTimedLockAlg_errorPrefix}: ${BemanTimedLockAlg_error}"
+                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
             )
         endif()
 
         if(method STREQUAL "FIND_PACKAGE")
-            if(package_name STREQUAL BemanTimedLockAlg_pkgName)
+            if(package_name STREQUAL BemanExemplar_pkgName)
                 string(
                     APPEND
-                    BemanTimedLockAlg_debug
-                    "Redirecting find_package calls for ${BemanTimedLockAlg_pkgName} "
+                    BemanExemplar_debug
+                    "Redirecting find_package calls for ${BemanExemplar_pkgName} "
                     "to FetchContent logic.\n"
                 )
                 string(
                     APPEND
-                    BemanTimedLockAlg_debug
-                    "Fetching ${BemanTimedLockAlg_repo} at "
-                    "${BemanTimedLockAlg_tag} according to ${BemanTimedLockAlg_lockfile}."
+                    BemanExemplar_debug
+                    "Fetching ${BemanExemplar_repo} at "
+                    "${BemanExemplar_tag} according to ${BemanExemplar_lockfile}."
                 )
-                message(DEBUG "${BemanTimedLockAlg_debug}")
+                message(DEBUG "${BemanExemplar_debug}")
                 FetchContent_Declare(
-                    "${BemanTimedLockAlg_name}"
-                    GIT_REPOSITORY "${BemanTimedLockAlg_repo}"
-                    GIT_TAG "${BemanTimedLockAlg_tag}"
+                    "${BemanExemplar_name}"
+                    GIT_REPOSITORY "${BemanExemplar_repo}"
+                    GIT_TAG "${BemanExemplar_tag}"
                     EXCLUDE_FROM_ALL
                 )
                 set(INSTALL_GTEST OFF) # Disable GoogleTest installation
-                FetchContent_MakeAvailable("${BemanTimedLockAlg_name}")
+                FetchContent_MakeAvailable("${BemanExemplar_name}")
 
                 # Important! <PackageName>_FOUND tells CMake that `find_package` is
                 # not needed for this package anymore
-                set("${BemanTimedLockAlg_pkgName}_FOUND" TRUE PARENT_SCOPE)
+                set("${BemanExemplar_pkgName}_FOUND" TRUE PARENT_SCOPE)
             endif()
         endif()
     endforeach()
 endfunction()
 
 cmake_language(
-    SET_DEPENDENCY_PROVIDER BemanTimedLockAlg_provideDependency
+    SET_DEPENDENCY_PROVIDER BemanExemplar_provideDependency
     SUPPORTED_METHODS FIND_PACKAGE
 )
 
